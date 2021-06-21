85b5c524337527ca519b1de51ba43ee4b83a53c6
yara89 <yara3001@gmail.com>
2021-06-21 16:40:00 +0300

    working tic tac toe final

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.value}>
       {props.value}
     </button>
   );
