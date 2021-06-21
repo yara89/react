ccc11ef662813dfe5066e036986db367349d394e
yara89 <yara3001@gmail.com>
2021-06-21 16:36:48 +0300

    working tic tac toe final

--- src/index.js
+++ src/index.js
@@ -57,7 +57,6 @@ class Game extends React.Component {
 
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
